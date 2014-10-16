8dba3ecf3c9e345b2009e329877e5169157a4303
Christopher Bupp <christopher.bupp@gmail.com>
2014-10-16 19:04:40 -0400

    adding print stencil

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,7 +14,7 @@
 	new L.tileLayer('http://{s}.sm.mapstack.stamen.com/($ff5f00[@p],(parks,$126600[source-in]),(mapbox-water,$00b688[source-in]),(terrain-lines,$99000f[source-in]),(terrain-labels,$662600[source-in])[soft-light])/{z}/{x}/{y}.png', {
 		minZoom: 0,
 		maxZoom: 18,
-		attribution: 'Tiles by Stamen Design, under CC-BY 3.0. Data © OpenStreetMap contributors, under CC-BY-SA. | Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+		attribution: 'Tiles by <a href="http://stamen.com/">Stamen Design</a>, under CC-BY 3.0. Data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
 	}).addTo(map);
 
 }(window, document, L));
